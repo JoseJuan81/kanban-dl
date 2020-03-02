00782cc2904da7a209ac8653205175624ebdab9b
josejuan81 <pe.dominguezjosejuan@gmail.com>
2020-03-02 06:56:00 -0500

    inicio configuracion basica de tablero

---
+++ src/serverless/dataBoard.js
@@ -0,0 +1,54 @@
+import { uuid } from '@/shared/utils.js';
+
+const columns = [
+	{
+		name: 'Pendiente',
+		id: `c-${uuid()}`,
+		tasks: [
+			{
+				description: '',
+				id: uuid(),
+				name: 'First task',
+				userAssigned: null,
+			},
+			{
+				description: '',
+				id: uuid(),
+				name: 'Second task',
+				userAssigned: null,
+			},
+			{
+				description: '',
+				id: uuid(),
+				name: 'Third task',
+				userAssigned: null,
+			},
+		],
+	},
+	{
+		name: 'En progreso',
+		id: `c-${uuid()}`,
+		tasks: [
+			{
+				description: '',
+				id: uuid(),
+				name: 'Fourth task',
+				userAssigned: null,
+			},
+		],
+	 },
+	 {
+		name: 'Hecho',
+		id: `c-${uuid()}`,
+		tasks: [
+			{
+				description: '',
+				id: uuid(),
+				name: 'fiveth task',
+				userAssigned: null,
+			},
+		],
+ 	}
+];
+
+export default columns;
---
+++ src/shared/utils.js
@@ -0,0 +1,3 @@
+const uuid = () => Math.random().toString(32).slice(2);
+
+export function uuid;
